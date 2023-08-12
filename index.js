768a785124e5fc69ff5563794d7e0226a0c68056
Santeri <santeri.ora@tuni.fi>
2023-08-12 23:56:22 +0300

    13.4

--- index.js
+++ index.js
@@ -1,17 +1,70 @@
-require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const express = require('express');
+const app = express();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+class Blog extends Model {}
+
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes:{
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+});
+
+Blog.sync();
+
+app.use(express.json());
 
 app.get('/api/blogs', async (req, res) => {
-  const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-  res.json(blogs)
-})
+  const blogs = await Blog.findAll();
+  console.log(JSON.stringify(blogs, null, 2));
+  res.json(blogs);
+});
 
-const PORT = process.env.PORT || 3001
+app.delete('/api/blogs/:id', async (req, res) => {
+  try {
+    await Blog.destroy({ where: { id: req.params.id }});
+    res.status(204);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    res.json(blog);
+  } catch(error) {
+    res.status(400).json({ error });
+  }
+});
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
 
